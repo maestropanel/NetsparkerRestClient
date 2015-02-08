3246e85ba1e407a30b2b205baada055190b80406
Emre ÇAĞLAR <emre52@gmail.com>
2015-02-08 13:25:50 +0200

    clear token

--- src/MaestroPanel.NetsparkerClient.Tests/WebsiteGroupTests.cs
+++ src/MaestroPanel.NetsparkerClient.Tests/WebsiteGroupTests.cs
@@ -14,7 +14,7 @@ namespace MaestroPanel.NetsparkerClient.Tests
         [TestMethod]
         public void websitegroup_should_be_default()
         {
-            var _client = new NetsparkerClient(new NetsparkerRestApi("https://www.netsparkercloud.com/api/1.0", "2ePd24tUZFgX2pladg7ZshHc0jNdM8jd4XOY70d8GnM="));
+            var _client = new NetsparkerClient(new NetsparkerRestApi("https://www.netsparkercloud.com/api/1.0", ""));
 
             var result = _client.WebSiteGroup()
                                 .List();
@@ -23,7 +23,7 @@ namespace MaestroPanel.NetsparkerClient.Tests
         [TestMethod]
         public void websitegroup_should_be_add()
         {
-            var _client = new NetsparkerClient(new NetsparkerRestApi("https://www.netsparkercloud.com/api/1.0", "2ePd24tUZFgX2pladg7ZshHc0jNdM8jd4XOY70d8GnM="));
+            var _client = new NetsparkerClient(new NetsparkerRestApi("https://www.netsparkercloud.com/api/1.0", ""));
 
             var result = _client.WebSiteGroup()
                                 .New(new NewWebsiteGroupApiModel
